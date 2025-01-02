5aac3aaeac261a5c19ad0cbc409dbfa363eb83c2
sajib37 <sajibhasan9811@gmail.com>
2025-01-03 00:40:38 +0600

    first commit

---
+++ Self/printFunction.py
@@ -0,0 +1,13 @@
+a=10
+b=2
+c=2
+age=25
+name= "sajib"
+
+print(a**b) #** is power operator
+print(a==b)
+print(c==b)
+print("My age is: ",age)
+#here we can format a print function string.
+print(f"My age is {age}.my name is {name}")
+print(type(name), type(age))
